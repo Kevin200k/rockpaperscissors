2c27e72c2502e8849f2cfb18227a41001dc4c1ca
Michael Amos <mikimiki2194@gmail.com>
2023-12-21 18:54:41 +0100

    javascript.js

--- javascript.js
+++ javascript.js
@@ -3,25 +3,39 @@ let ComputerChoice =  function(){
     let randomValue = rockPaperScissors[Math.floor(Math.random() * 3) + 1];
     return randomValue;
 };
-let playerSelection = prompt("Pick either Rock, Paper, or Scissors")
+let playerSelection = prompt("Pick either Rock, Paper, or Scissors");
 
-playerSelection = playerSelection.toLowerCase();
+
 function singleRound(){
     let computerChoice = ComputerChoice().toLowerCase();
-    if((playerSelection == computerChoice)){
+    if((playerSelection.toLowerCase() == computerChoice)){
         return "TIE!"
-    } else if((playerSelection == "rock" && computerChoice == "paper")){
+    } else if((playerSelection.toLowerCase() == "rock" && computerChoice == "paper")){
         return "You lose, Paper beats Rock"
-    } else if((playerSelection == "paper" && computerChoice == "rock")){
+    } else if((playerSelection.toLowerCase() == "paper" && computerChoice == "rock")){
         return "You win, Paper beats Rock"
-    } else if((playerSelection == "rock" && computerChoice == "scissors")){
+    } else if((playerSelection.toLowerCase() == "rock" && computerChoice == "scissors")){
         return "You win, Rock beats Scissors"
-    } else if((playerSelection == "scissors" && computerChoice == "rock")){
+    } else if((playerSelection.toLowerCase() == "scissors" && computerChoice == "rock")){
         return "You lose, Rock beats Scissors"
-    } else if(playerSelection == "scissors" && computerChoice == "paper"){
+    } else if(playerSelection.toLowerCase() == "scissors" && computerChoice == "paper"){
         return "You win, Scissors beats Paper"
-    } else if(playerSelection == "paper" && computerChoice == "scissors"){
+    } else if(playerSelection.toLowerCase() == "paper" && computerChoice == "scissors"){
         return "You lose, Scissors beats Paper"
+    } else{
+        return "Try again"
     }
     }
-alert(singleRound())
+function game (){
+    alert(singleRound());
+    playerSelection = prompt("Pick either Rock, Paper, or Scissors");
+    alert(singleRound());
+    playerSelection = prompt("Pick either Rock, Paper, or Scissors");
+    alert(singleRound());
+    playerSelection = prompt("Pick either Rock, Paper, or Scissors");
+    alert(singleRound());
+    playerSelection = prompt("Pick either Rock, Paper, or Scissors");
+    alert(singleRound());
+}
+
+game();
