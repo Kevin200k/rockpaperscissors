6c54369f47dc2930def52cb9ec47bc7ee704cb89
Michael Amos <mikimiki2194@gmail.com>
2023-12-21 17:46:44 +0100

    javascript.js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,27 @@
-function ComputerChoice () {
+let ComputerChoice =  function(){
     const rockPaperScissors = [" ","Rock","Paper","Scissors"];
     let randomValue = rockPaperScissors[Math.floor(Math.random() * 3) + 1];
-    alert(randomValue);
-}
-ComputerChoice();
+    return randomValue;
+};
+let playerSelection = prompt("Pick either Rock, Paper, or Scissors")
 
+playerSelection = playerSelection.toLowerCase();
+function singleRound(){
+    let computerChoice = ComputerChoice().toLowerCase();
+    if((playerSelection == computerChoice)){
+        return "TIE!"
+    } else if((playerSelection == "rock" && computerChoice == "paper")){
+        return "You lose, Paper beats Rock"
+    } else if((playerSelection == "paper" && computerChoice == "rock")){
+        return "You win, Paper beats Rock"
+    } else if((playerSelection == "rock" && computerChoice == "scissors")){
+        return "You win, Rock beats Scissors"
+    } else if((playerSelection == "scissors" && computerChoice == "rock")){
+        return "You lose, Rock beats Scissors"
+    } else if(playerSelection == "scissors" && computerChoice == "paper"){
+        return "You win, Scissors beats Paper"
+    } else if(playerSelection == "paper" && computerChoice == "scissors"){
+        return "You lose, Scissors beats Paper"
+    }
+    }
+alert(singleRound())
