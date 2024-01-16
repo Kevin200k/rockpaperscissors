dfdd0c6ddf06af707cde7ed0d7bd05023fe52901
Michael Amos <mikimiki2194@gmail.com>
2024-01-16 12:58:48 +0100

    javascript.js

--- javascript.js
+++ javascript.js
@@ -9,41 +9,42 @@ let scissorsValue = document.querySelector("#scissors");
 let paperValue = document.querySelector("#paper");
 
 function playerSelection(id){
+    console.log('here'+id)
     let element = document.getElementById(id)
 
     if(element) {
+        let res = element.innerText
+        console.log('res'+res)
+
         return element.innerText;
     };
-}
+};
 
-rockValue.addEventListener("click", function(){
-    playerSelection("rock")});
-paperValue.addEventListener("click", function(){ 
-    playerSelection("paper")});
-scissorsValue.addEventListener("click", function(){
-    playerSelection("scissors")});
-
-
-function singleRound(){
+function singleRound(id){
     let computerChoice = ComputerChoice();
-    if((playerSelection() == computerChoice)){
-        return "TIE!"
-    } else if((playerSelection("rock") && computerChoice == "paper")){
-        return "You lose, Paper beats Rock"
-    } else if((playerSelection("paper") && computerChoice == "rock")){
-        return "You win, Paper beats Rock"
-    } else if((playerSelection("rock") && computerChoice == "scissors")){
-        return "You win, Rock beats Scissors"
-    } else if((playerSelection("scissors") && computerChoice == "rock")){
-        return "You lose, Rock beats Scissors"
-    } else if(playerSelection("scissors")&& computerChoice == "paper"){
-        return "You win, Scissors beats Paper"
-    } else if(playerSelection("paper")&& computerChoice == "scissors"){
-        return "You lose, Scissors beats Paper"
-    } else{
-        return "Try again"
+    // let playerChoice = playerSelection("rock")||playerSelection("paper")||playerSelection("scissors");
+    let playerChoice = playerSelection(id);
+    if((playerChoice === computerChoice)){
+        return `You chose ${playerChoice}, and the Computer chose ${computerChoice}. TIE!`
+    } else if(playerChoice === "Rock" && computerChoice === "Paper"||
+              playerChoice === "Paper" && computerChoice ==="Scissors"||
+              playerChoice === "Scissors" && computerChoice === "Rock") {
+                return `You chose ${playerChoice}, and the Computer chose ${computerChoice}. You lose!`
+              } else if((playerChoice === "Paper" && computerChoice === "Rock"||
+                         playerChoice === "Scissors" && computerChoice ==="Paper"||
+                         playerChoice === "Rock" && computerChoice === "Scissors")){
+                    return `You chose ${playerChoice}, and the Computer chose ${computerChoice}. You win!`
     }};
 
+
+function playerRound(id){
     let demo = document.querySelector(".demo");
-    demo.innerText = singleRound();
+    
+    demo.innerText = singleRound(id)
+    };
+    
+// rockValue.addEventListener("click", playerRound);
+// paperValue.addEventListener("click", playerRound);
+// scissorsValue.addEventListener("click", playerRound);
+
 
