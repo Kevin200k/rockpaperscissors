67f0f4f74f92731f05d3c954d5749a02eceaf399
Michael Amos <mikimiki2194@gmail.com>
2024-01-19 09:52:45 +0100

    javascript.js

--- javascript.js
+++ javascript.js
@@ -9,49 +9,79 @@ let scissorsValue = document.querySelector("#scissors");
 let paperValue = document.querySelector("#paper");
 
 function playerSelection(id){
-    console.log('here'+id)
     let element = document.getElementById(id)
 
     if(element) {
         return element.innerText;
     };
 };
+ 
+
 
 function singleRound(id){
-    let computerChoice = ComputerChoice();
     // let playerChoice = playerSelection("rock")||playerSelection("paper")||playerSelection("scissors");
     let playerChoice = playerSelection(id);
+    let computerChoice = ComputerChoice();
     if((playerChoice === computerChoice)){
-        return `You chose ${playerChoice}, and the Computer chose ${computerChoice}. TIE!`
+        // return `You chose ${playerChoice}, and the Computer chose ${computerChoice}. TIE!`
+        return ( {'result': 'TIE!', 'resultMessage':`You chose ${playerChoice}, and the Computer chose ${computerChoice}. TIE!`});
+
     } else if(playerChoice === "Rock" && computerChoice === "Paper"||
               playerChoice === "Paper" && computerChoice ==="Scissors"||
               playerChoice === "Scissors" && computerChoice === "Rock") {
-                return `You chose ${playerChoice}, and the Computer chose ${computerChoice}. You lose!`
-              } else if((playerChoice === "Paper" && computerChoice === "Rock"||
-                         playerChoice === "Scissors" && computerChoice ==="Paper"||
-                         playerChoice === "Rock" && computerChoice === "Scissors")){
-                    return `You chose ${playerChoice}, and the Computer chose ${computerChoice}. You win!`
-    }};
+                // return ( `You chose ${playerChoice}, and the Computer chose ${computerChoice}. You lose!`);
+                return ( {'result': 'You lose', 'resultMessage':`You chose ${playerChoice}, and the Computer chose ${computerChoice}. You lose!`});
+              } else {
+            //    return (`You chose ${playerChoice}, and the Computer chose ${computerChoice}. You win!`);
+               return ( {'result': 'You win', 'resultMessage':`You chose ${playerChoice}, and the Computer chose ${computerChoice}. You win!`});
+
+              }};
+
+    ;
+let playerScoreValue = document.getElementById("playerScoreValue");
+let computerScoreValue = document.getElementById("computerScoreValue");
+
+let i = parseInt(playerScoreValue.innerText);
+let j = parseInt(computerScoreValue.innerText);
+
 function playerRound(id){
     let demo = document.querySelector(".demo");
+    let res = singleRound(id);
+    console.log('res ='+res.result)
+    // do something
+    demo.innerText = res.resultMessage
     
-    demo.innerText = singleRound(id)
-    };
 
-function score(singleRound){
-    let playerScore = document.querySelector("#playerScore");
-    let computerScore = document.querySelector("#computerScore");
+    if (i <= 4 && j <= 4) {
+        if (res.result === "You win") {
+            playerScoreValue.innerText = ++i;
+            return playerScoreValue.innerText;
+        } else if (res.result === "You lose") {
+            computerScoreValue.innerText = ++j;
+            return computerScoreValue.innerText;
+        }
 
-    let j = playerScore.innerText;
-    let i = computerScore.innerText;
-    
-};
-    
-score();
+        // break
+     } else if((i === 5 && !(j === 5))||(!(i === 5) && j === 5)){
+        if (i < j) {
+            console.log(i);
+            demo.innerText = "You lose"  
+
+        } else if(i > j){
+            console.log(j);
+              demo.innerText = "Congratulations, You Win"
+          }};
+        };
 
     
-// rockValue.addEventListener("click", playerRound);
+
+
+// rockValue.addEvyGmaentListener("click", playerRound);
 // paperValue.addEventListener("click", playerRound);
 // scissorsValue.addEventListener("click", playerRound);
+
+// singleRound() === `You chose Rock, and the Computer chose Paper. You lose!`||
+// singleRound() === `You chose Paper, and the Computer chose Scissors. You lose!`||
+// singleRound() === `You chose Scissors, and the Computer chose Rock. You lose!`
 
 
