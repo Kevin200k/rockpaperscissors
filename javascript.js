5ff4e7fb26e043e6fae90cb7af4e760009fb83aa
Michael Amos <mikimiki2194@gmail.com>
2024-02-21 11:17:43 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -57,16 +57,14 @@ let j = parseInt(computerScoreValue.innerText);
 
 let scoreOne = 0; // Player Score
 let scoreTwo = 0; // Computer Score
-let finalResult;
+let demo = document.querySelector(".demo");
 
 function playerRound(id){
-    let demo = document.querySelector(".demo");
     let res = singleRound(id);
-
     demo.innerText = res.resultMessage
     
 
-    if (i <4 && j <= 4) {
+    if (i <4 && j < 4) {
         if (res.result === "You win") {
             scoreOne= ++i;
             playerScoreValue.innerText = scoreOne;
@@ -77,35 +75,69 @@ function playerRound(id){
             return computerScoreValue.innerText;
         }
     }else if (i === 4 || j === 4) {
-        if (res.result === "You win") {
+        if (i <4 || j < 4) {
+            if (res.result === "You win") {
+                scoreOne= ++i;
+                playerScoreValue.innerText = scoreOne;
+                return playerScoreValue.innerText;
+            } else if (res.result === "You lose") {
+                scoreTwo = ++j;
+                computerScoreValue.innerText = scoreTwo;
+                return computerScoreValue.innerText;
+        }}
+        else if (res.result === "You win") {
             scoreOne= ++i;
-            playerScoreValue.innerText = scoreOne;
-            return playerScoreValue.innerText;
+            if(scoreOne === 5){
+                playerScoreValue.innerText = scoreOne;
+                button.style.display = "none";
+                playAgain.style.display = "block";
+                demo.setAttribute("style", "font-size:40px; color:hsla(52, 100%, 50%); text-shadow: 3px 3px 10px;");
+                demo.innerText = 'Congratulations, You Win';
+            }
+           
         } else if (res.result === "You lose") {
             scoreTwo = ++j;
-            computerScoreValue.innerText = scoreTwo;
-            return computerScoreValue.innerText;
+           if(scoreTwo === 5){
+                computerScoreValue.innerText = scoreTwo;
+                button.style.display = "none";
+                playAgain.style.display = "block";
+                demo.setAttribute("style", "font-size:40px; color:hsla(52, 100%, 50%); text-shadow: 3px 3px 10px;");
+                demo.innerText = 'Sorry, Lose';
+                ;
+           } else if((res.result === "TIE!")){
+                scoreOne = i;
+                scoreTwo = j;
+           }
         }}
-     };
-        
-console.log(scoreOne);
-    function tryAgain(){
-        // if((i === 5 && !(j === 5))||(!(i === 5) && j === 5)){
-        //     if (i < j) {
-        //         console.log(scoreOne);
-        //         finalResult = "Sorry, You lose" ;
-        //         demo.innerText = finalResult  
-    
-        //     } else if(i > j){
-        //         console.log(scoreTwo);
-        //         finalResult  = "Congratulations, You Win"
-        //         demo.innerText = finalResult 
-        //       }};
-        if(playerScoreValue.innerText === 5 ){
-            console.log("Hello World")
-        }
-            };
+    // }else if(i === 5 && !(j === 5) || j === 5 && !(i === 5)){
+    //         finalResult();
+    //     }
+    };
+
+let button = document.querySelector("#button");
+let playAgain = document.querySelector(".playAgain")
+
+function finalResult(){
+    button.style.display = "none";
+    console.log("Micheal2194$")
+    playAgain.style.display = "block";
+
     tryAgain();
+}
+function tryAgain(){
+    
+    i = 0;
+    j = 0;
+    scoreOne = 0;
+    scoreTwo = 0;
+    
+    playerScoreValue.innerText = scoreOne;
+    computerScoreValue.innerText = scoreTwo;
+    demo.innerText = "";
+    
+    button.style.display = "flex";
+    playAgain.style.display = "none"
+            };
 
 let container = document.querySelector(".container");
 let dialogue = document.querySelector(".dialogue")
